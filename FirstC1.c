4ea0bd4dfd38a66a2c89cda86c630b4f8a067208
IVAN HRYNEVICH <kodshrob@gmail.com>
2020-09-15 12:42:57 +0300

    Program than sums two integers
    int integer1, integer2, sum;

--- FirstC1.c
+++ FirstC1.c
@@ -1,5 +1,12 @@
-//first C programm
+/* Program that sums two integers */
+#include <stdio.h>
 main(){
-	printf("Hello world \a \n");
+	int integer1, integer2, sum;
+	printf("Enter the first integer \n");
+	scanf("%d", &integer1);
+	printf("Insert the second integer \n");
+	scanf("%d", &integer2);
+	sum = integer1 + integer2;
+	printf("Their sum is %d\n", sum);
 	return 0;
 }
