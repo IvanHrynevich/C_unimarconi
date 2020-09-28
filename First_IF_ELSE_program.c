da3fe8ab93bdf32709f2e4a87dae714f48f2766f
IVAN HRYNEVICH <kodshrob@gmail.com>
2020-09-29 02:14:05 +0300

    Add IF/ELSE first program in C language

---
+++ First_IF_ELSE_program.c
@@ -0,0 +1,17 @@
+///*First IF/ELSE program*/
+#include <stdio.h>
+
+main(){
+	int mark_cent;
+	float mark_dec;
+	printf("Enter the mark from 1 to 100\n");
+	scanf("%d", &mark_cent);
+	if (mark_cent >= 60) {
+		mark_dec - mark_cent * 10.0 / 100.0;
+		printf("Accepted!\n");
+		printf("Mark (1-10) is %f\n", mark_dec);
+	}
+	else printf("Failure!\n");
+	system("PAUSE");//console don't close after that command
+	return 0; /*The program completed successfully*/
+}
