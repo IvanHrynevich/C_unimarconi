bb401f40a2531b1d8492d94d45573542406e600a
IVAN HRYNEVICH <kodshrob@gmail.com>
2020-09-29 03:18:18 +0300

    Add if/else nested

--- IF_ELSE_NESTED.c
+++ IF_ELSE_NESTED.c
@@ -3,15 +3,31 @@
 
 main(){
 	int mark;
-	float mark_dec;
+	int year;
 	printf("Enter the mark from 1 to 100\n");
-	scanf("%d", &mark_cent);
-	if (mark_cent >= 60) {
-		mark_dec - mark_cent * 10.0 / 100.0;
+	scanf("%d", &mark);
+	printf("Enter the year of ending univercity\n");
+	scanf("%d", &year);
+		if ((mark >= 60) && (year >= 1997)) {
 		printf("Accepted!\n");
-		printf("Mark (1-10) is %f\n", mark_dec);
+		if (mark >= 90) printf("Mark A\n");
+		else {
+			if (mark >= 80) printf("Mark B\n");
+			else {
+				if (mark >= 70) printf("Mark C\n");
+				else printf("Mark D\n");
+			}
+		}
 	}
-	else printf("Failure!\n");
+	else {
+		if ((mark < 60) && (year >= 1997)) printf("Failure!\n");
+		else {
+			if ((mark >= 36) && (year < 1997)) printf("Accepted!\n");
+			else {
+				if ((mark < 36) && (year < 1997)) printf("Failure!\n");
+			}
+		}
+	}
 	system("PAUSE");//console don't close after that command
 	return 0; /*The program completed successfully*/
 }
